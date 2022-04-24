e5b02b98eaa419006258cd04032b3b7b423a546f
Leonardo <leonardoamelo3@gmail.com>
2022-04-24 19:19:09 -0300

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { EditModalComponent } from '../edit-modal/edit-modal.component';
+
+export class TaskInterface {
+  id: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  public tasks: TaskInterface[] = [];
+  public taskName: string = '';
+
+  constructor(
+    private modalController: ModalController
+  ) { }
+
+  insertTask(name: string) {
+    const id = String(new Date().getTime());
+
+    this.tasks.push({ name, id });
+
+    this.taskName = '';
+  }
+
+  deleteTask(id: string) {
+    const newTasksArray: TaskInterface[] = this.tasks.filter((el: TaskInterface) => el.id != id);
+
+    this.tasks = newTasksArray;
+  }
+
+
+}
